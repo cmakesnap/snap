44e54cd3f11fff75714b8e88c8a1f40693e741c0
Kyle Heath <cmakesnap@gmail.com>
2013-03-20 00:08:28 +0000

    Adds the SYS-libunwind package.

---
+++ sys/FindSYS-libunwind.cmake
@@ -0,0 +1,8 @@
+FIND_SNAP_SYS_PACKAGE(
+  NAME                  SYS-libunwind
+  LIBRARY_NAMES         libunwind.a
+  LIBRARY_SEARCH_PATHS  /usr/local/lib/                        
+  PATH_TO_A_HEADER      libunwind.h
+  INCLUDE_SEARCH_PATHS  /usr/local/include                        
+)
+
