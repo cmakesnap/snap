00ee55f9a1e794c6db1651c5ef4dfc6c72c5598e
Kyle Heath <cmakesnap@gmail.com>
2013-04-27 03:19:23 +0000

    Adjusts the default compiler warning settings to be less noisy.

--- internal/cpp_compiler_tweaks.cmake
+++ internal/cpp_compiler_tweaks.cmake
@@ -2,20 +2,40 @@
 
 SET(SNAP_CPP_WARNING_LEVEL 1 CACHE STRING "0 = no warnings, 1 = basic warnings, 2 = basic warnings + error on some warns, 3 = agressive warnings and all warns are errors")
   
-#add_definitions(-ferror-limit=10)
-  
 #No warnings
 IF (${SNAP_CPP_WARNING_LEVEL} EQUAL 0)    
-#Basic warnings
+#Basic warnings and some warnings that are common problems are errors
 ELSEIF (${SNAP_CPP_WARNING_LEVEL} EQUAL 1)
-  ADD_DEFINITIONS(-Wall)    
+  ADD_DEFINITIONS(-Wall)
+  
+  # disable some harmless warnings that just create noise    
   ADD_DEFINITIONS(-Wno-sign-compare)
+  ADD_DEFINITIONS(-Wno-write-strings)
+  ADD_DEFINITIONS(-Wno-self-assign)
+  ADD_DEFINITIONS(-Wno-deprecated)
+  ADD_DEFINITIONS(-Wno-unused-function)
+  ADD_DEFINITIONS(-Wno-unused-variable)
+  ADD_DEFINITIONS(-Wno-unneeded-internal-declaration)
+  ADD_DEFINITIONS(-Wno-overloaded-virtual)
+  
+  # Turn some warnings into errors
   ADD_DEFINITIONS(-Werror=return-type)  
   ADD_DEFINITIONS(-Werror=invalid-offsetof)
-#Basic warnings and some warnings that are common problems are errors
+#Basic warnings and some more warnings that are common problems are errors
 ELSEIF (${SNAP_CPP_WARNING_LEVEL} EQUAL 2)
   ADD_DEFINITIONS(-Wall)
+  
+  # disable some harmless warnings that just create noise    
   ADD_DEFINITIONS(-Wno-sign-compare)
+  ADD_DEFINITIONS(-Wno-write-strings)
+  ADD_DEFINITIONS(-Wno-self-assign)
+  ADD_DEFINITIONS(-Wno-deprecated)
+  ADD_DEFINITIONS(-Wno-unused-function)
+  ADD_DEFINITIONS(-Wno-unused-variable)
+  ADD_DEFINITIONS(-Wno-unneeded-internal-declaration)
+  ADD_DEFINITIONS(-Wno-overloaded-virtual)
+  
+  # Turn some warnings into errors
   ADD_DEFINITIONS(-Werror=return-type)    
   ADD_DEFINITIONS(-Werror=invalid-offsetof)
   ADD_DEFINITIONS(-Werror=uninitialized) 
